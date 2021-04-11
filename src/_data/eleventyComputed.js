d3bb53a049c1e0f72c106ad05b1de819f09b8924
Carlos Souza <carlos.pessoal@hotmail.com>
2021-04-11 19:46:27 -0300

    Ajustes dos links

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -1,4 +1,9 @@
+const local = 'http://localhost:8080'
+const domainPath = 'https://carlos8v.github.io/md-talk';
+const DEBUB = process.env.MODE === 'development';
+
 module.exports = {
+  domain: DEBUB ? local : domainPath,
   rootPath: function(data) {
     return data.page.url
       .split('/')
